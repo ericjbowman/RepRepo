fbc157ee3716dcc314eb3351ef4a4e0cbf3b243e
ericbowman <ericjbowman@gmail.com>
2019-06-07 23:29:08 -0400

    Add font to App name

--- assets/scripts/ui.js
+++ assets/scripts/ui.js
@@ -3,8 +3,8 @@ const api = require('./api')
 
 const signUpSuccess = function (data) {
   $('#log-message').html('Signed up!')
+  $('form').trigger('reset')
 }
-
 
 const showMasterTunes = function () {
   let display = ''
@@ -15,12 +15,13 @@ const showMasterTunes = function () {
 }
 
 const signInSuccess = function (data) {
+  $('form').trigger('reset')
   $('#sign-up').hide()
   $('#sign-in').hide()
   $('.step-one').hide()
   $('#full-rep').addClass('selected')
   store.user = data.user
-  $('#log-message').html('Signed in!')
+  // $('#log-message').html('Signed in!')
   api.indexMasterTunes()
     .then((index) => {
       store.masterTunes = index.master_tunes
@@ -35,6 +36,7 @@ const signOutSuccess = function () {
   $('#sign-up').show()
   $('#sign-in').show()
   $('#log-message').html('Signed out!')
+  $('#full-rep').removeClass('selected')
 }
 
 module.exports = {
